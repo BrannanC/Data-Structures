6eb9885024c5c5f79e7ec11c388efea81f384938
BrannanC <brannanconrad@gmail.com>
2019-09-19 14:07:17 -0500

    new binary search tree stuff

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,3 +1,6 @@
+from dll_queue import Queue
+from dll_stack import Stack
+
 
 class BinarySearchTree:
     def __init__(self, value):
@@ -44,3 +47,47 @@ class BinarySearchTree:
             self.left.for_each(cb)
         if self.right != None:
             self.right.for_each(cb)
+
+    # DAY 2 Project -----------------------
+
+    # Print all the values in order from low to high
+    # Hint:  Use a recursive, depth first traversal
+    def in_order_dft(self, node):
+        if node.left:
+            node.in_order_dft(node.left)
+        print(node.value)
+        if node.right:
+            node.in_order_dft(node.right)
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative breadth first traversal
+    def bft_print(self, node):
+        pass
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative depth first traversal
+    def dft_print(self, node):
+        pass
+
+    # STRETCH Goals -------------------------
+    # Note: Research may be required
+
+    # Print In-order recursive DFT
+    def pre_order_dft(self, node):
+        pass
+
+    # Print Post-order recursive DFT
+    def post_order_dft(self, node):
+        pass
+
+
+bst = BinarySearchTree(1)
+bst.insert(8)
+bst.insert(5)
+bst.insert(7)
+bst.insert(6)
+bst.insert(3)
+bst.insert(4)
+bst.insert(2)
+
+bst.in_order_dft(bst)
---
+++ binary_search_tree/dll_queue.py
@@ -0,0 +1,23 @@
+from doubly_linked_list import DoublyLinkedList
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        self.storage = DoublyLinkedList()
+
+    def enqueue(self, value):
+        self.storage.add_to_tail(value)
+        self.size += 1
+
+    def dequeue(self):
+        if self.size == 0:
+            return None
+        else:
+            last = self.storage.remove_from_head()
+            self.size -= 1
+            return last
+
+    def len(self):
+        return self.size
