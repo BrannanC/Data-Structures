cb137e17bb4f8ef32ed589579b5de2fb5e7bdfeb
BrannanC <brannanconrad@gmail.com>
2020-07-28 15:03:05 -0500

    generic heap

--- heap/generic_heap.py
+++ heap/generic_heap.py
@@ -1,22 +1,64 @@
 class Heap:
-  def __init__(self, comparator):
-    self.storage = []
-    self.comparator = comparator
+    def __init__(self, compare, arr=[]):
+        self.heap = self.construct(arr)
+        self.compare = compare
 
-  def insert(self, value):
-    pass
+    def __len__(self):
+        return len(self.heap)
 
-  def delete(self):
-    pass
+    def construct(self, arr):
+        if not len(arr):
+            return arr
+        parent_ind = (len(arr) - 1) // 2
+        for i in reversed(range(parent_ind)):
+            self.sift_down(i, len(arr)-1, arr)
+        return arr
 
-  def get_priority(self):
-    pass
+    def sift_down(self, i, end, heap):
+        left_child = i * 2 + 1
+        while left_child <= end:
+            right_child = i * 2 + 2 if i * 2 + 2 <= end else None
+            if right_child is not None:
+                if self.compare(heap[right_child], heap[left_child]):
+                    temp = right_child
+                else:
+                    temp = left_child
+            else:
+                temp = left_child
+            if self.compare(heap[temp], heap[i]):
+                heap[temp], heap[i] = heap[i], heap[temp]
+                i = temp
+                left_child = i * 2 + 1
+            else:
+                return
 
-  def get_size(self):
-    pass
+    def sift_up(self, i, heap):
+        parent = (i - 1) // 2
+        while i > 0:
+            if self.compare(heap[i], heap[parent]):
+                heap[i], heap[parent] = heap[parent], heap[i]
+                i = parent
+                parent = (i - 1) // 2
+            else:
+                return
 
-  def _bubble_up(self, index):
-    pass
+    def peek(self):
+        return self.heap[0]
 
-  def _sift_down(self, index):
-    pass
+    def remove(self):
+        self.heap[0], self.heap[-1] = self.heap[-1], self.heap[0]
+        val = self.heap.pop()
+        self.sift_down(0, len(self)-1, self.heap)
+        return val
+
+    def insert(self, val):
+        self.heap.append(val)
+        self.sift_up(len(self)-1, self.heap)
+
+
+def max_heap_func(a, b):
+    return a > b
+
+
+def min_heap_func(a, b):
+    return a < b
